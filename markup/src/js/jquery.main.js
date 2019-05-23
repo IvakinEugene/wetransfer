6c1e307a6b189836764a01df0103013829afba60
Ivakin Eugene <yevgeniy.ivakin@catenamedia.com>
2019-05-23 19:06:51 +0300

    no message

--- markup/src/js/jquery.main.js
+++ markup/src/js/jquery.main.js
@@ -122,6 +122,22 @@ function initSlick() {
         infinite: false,
         arrows: false
     });
+    $('.product-slider .slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-nav'
+    });
+    $('.product-slider .slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: true,
+        // centerMode: true,
+        focusOnSelect: true,
+        variableWidth: true
+    });
 }
 
 /*
